8ff0bc3d2bb038179d563d55054128c0ed2e93d2
Daniel Perez <dperez@soaint.com>
2021-04-13 10:52:42 -0400

    feat: Add routes to services

--- reservations-backend/routes/api.php
+++ reservations-backend/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('contactTypes', ContactTypeController::class);
+Route::resource('contacts', ContactController::class);
+Route::resource('reservations', ReservationController::class);
