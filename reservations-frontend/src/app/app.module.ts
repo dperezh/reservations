c59c2a5fac1a4dde9ddc732a5e53b52ef9831444
Daniel Perez <dperez@soaint.com>
2021-04-15 14:26:27 -0400

    [FRONT] - refactor: Update the app component

--- reservations-frontend/src/app/app.module.ts
+++ reservations-frontend/src/app/app.module.ts
@@ -1,20 +1,70 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// Components
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { ReservationListComponent } from './views/reservation-list/reservation-list.component';
+import { ReservationCreateComponent } from './views/reservation-create/reservation-create.component';
+import { ContactListComponent } from './views/contact-list/contact-list.component';
+import { ContactCreateComponent } from './views/contact-create/contact-create.component';
+
+// Modules
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { InputMaskModule } from 'primeng/inputmask';
+import { CardModule } from 'primeng/card';
+import { EditorModule } from 'primeng/editor';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    ReservationListComponent,
+    ReservationCreateComponent,
+    ContactListComponent,
+    ContactCreateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    TableModule,
+    RatingModule,
+    FormsModule,
+    InputTextModule,
+    DropdownModule,
+    CalendarModule,
+    InputMaskModule,
+    CardModule,
+    EditorModule,
+    AutoCompleteModule,
+    MessageModule,
+    MessagesModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
