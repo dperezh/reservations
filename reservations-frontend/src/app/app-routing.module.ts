dd669231592bc2e215b842dcc81bd1f773c2c909
Daniel Perez <dperez@soaint.com>
2021-04-15 14:26:51 -0400

    [FRONT] - feat: Add routing

--- reservations-frontend/src/app/app-routing.module.ts
+++ reservations-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReservationCreateComponent } from './views/reservation-create/reservation-create.component';
+import { ReservationListComponent } from './views/reservation-list/reservation-list.component';
+import { ContactListComponent } from './views/contact-list/contact-list.component';
+import { ContactCreateComponent } from './views/contact-create/contact-create.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'reservations', component: ReservationListComponent },
+  { path: 'create-reservation', component: ReservationCreateComponent },
+  { path: 'contacts', component: ContactListComponent },
+  { path: 'create-contact', component: ContactCreateComponent },
+  { path: '**', redirectTo: '/reservations'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
